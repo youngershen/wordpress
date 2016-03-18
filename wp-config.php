9f8d8a9b0fb4266537d2b10c23994dcfd32db1ff
youngershen <younger.x.shen@gmail.com>
2016-03-18 14:36:00 +0800

    init

---
+++ wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * WordPress基础配置文件。
+ *
+ * 这个文件被安装程序用于自动生成wp-config.php配置文件，
+ * 您可以不使用网站，您需要手动复制这个文件，
+ * 并重命名为“wp-config.php”，然后填入相关信息。
+ *
+ * 本文件包含以下配置选项：
+ *
+ * * MySQL设置
+ * * 密钥
+ * * 数据库表名前缀
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/zh-cn:%E7%BC%96%E8%BE%91_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
+/** WordPress数据库的名称 */
+define('DB_NAME', 'youngershen');
+
+/** MySQL数据库用户名 */
+define('DB_USER', 'root');
+
+/** MySQL数据库密码 */
+define('DB_PASSWORD', 'root');
+
+/** MySQL主机 */
+define('DB_HOST', 'localhost');
+
+/** 创建数据表时默认的文字编码 */
+define('DB_CHARSET', 'utf8mb4');
+
+/** 数据库整理类型。如不确定请勿更改 */
+define('DB_COLLATE', '');
+
+/**#@+
+ * 身份认证密钥与盐。
+ *
+ * 修改为任意独一无二的字串！
+ * 或者直接访问{@link https://api.wordpress.org/secret-key/1.1/salt/
+ * WordPress.org密钥生成服务}
+ * 任何修改都会导致所有cookies失效，所有用户将必须重新登录。
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'ClX3*7QtwY`UX9c+:^VmE+ pXz>TI*iG+:at-F}`ann5Ik$-(cHVOVv+q[Fr{rdP');
+define('SECURE_AUTH_KEY',  'QIuK$Ku&;?-CV=K*kaN6DUUK<szUG@94z:&b-vPr,Sh&F6&jbk]r]T0zsI@+r9&t');
+define('LOGGED_IN_KEY',    '={IF.J((6(BMukH-FgYaos!WZ-xw!>e_BJD]@@L_Z1+7m::[[}-w@X7,+c^SKLEO');
+define('NONCE_KEY',        'KWl&2h|_-+u}xe:K%Gvs8D<ygGZs4Hy^_:VX! ]}k%QyKbB]uuJ6LfpxOF|A2+i ');
+define('AUTH_SALT',        'h8DR+{Ne+$IbH[+HIY#^<RUg%QpNZg~DFIkZxO,.+mZW./i_5lq z&p.g,+:LsS{');
+define('SECURE_AUTH_SALT', '=ibN^;K5{_-!Sz1*~Q.@SG*?-H~yn{z?e(ZMPqxC]KCF1GVg:dp0)|}EUM|OO66*');
+define('LOGGED_IN_SALT',   'G<d=I+>wVIu!(LwGi)yv:Mnxu{PSithR*JQPuVWaY@mu}&b_!m>iom`aVy+0%zyN');
+define('NONCE_SALT',       'DYR2^TAKDOQ+^7n{kCw-C4GCKOmedhRZFBjz>ZJ^K1[SKx0lC&=P>WaSkuvJ+84#');
+
+/**#@-*/
+
+/**
+ * WordPress数据表前缀。
+ *
+ * 如果您有在同一数据库内安装多个WordPress的需求，请为每个WordPress设置
+ * 不同的数据表前缀。前缀名只能为数字、字母加下划线。
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * 开发者专用：WordPress调试模式。
+ *
+ * 将这个值改为true，WordPress将显示所有用于开发的提示。
+ * 强烈建议插件开发者在开发环境中启用WP_DEBUG。
+ *
+ * 要获取其他能用于调试的信息，请访问Codex。
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/**
+ * zh_CN本地化设置：启用ICP备案号显示
+ *
+ * 可在设置→常规中修改。
+ * 如需禁用，请移除或注释掉本行。
+ */
+define('WP_ZH_CN_ICP_NUM', true);
+
+/* 好了！请不要再继续编辑。请保存本文件。使用愉快！ */
+
+/** WordPress目录的绝对路径。 */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** 设置WordPress变量和包含文件。 */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/plugins/useso-take-over-google/useso-take-over-google.php
@@ -0,0 +1,139 @@
+<?php
+/*
+Plugin Name: Useso take over Google
+Plugin URI: http://www.brunoxu.com/useso-take-over-google.html
+Description: 用360前端公共库Useso接管Google字体库和Google公共库，无需设置，插件安装激活后即刻生效。
+Author: Bruno Xu
+Author URI: http://www.brunoxu.com/
+Version: 1.6.4
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+define('USESO_TAKE_OVER_GOOGLE_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+define('USESO_TAKE_OVER_GOOGLE_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
+
+function useso_take_over_google_is_login_page() {
+	return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
+}
+
+if (is_admin()) {// init -> wp_loaded -> admin_menu -> admin_init -> wp -> admin_enqueue_scripts -> admin_head
+	$action = 'admin_init'; // OK
+	//$action = 'wp'; // NG
+} elseif (useso_take_over_google_is_login_page()) {
+	$action = 'wp_loaded'; // OK
+	//$action = 'wp'; // NG
+} else { // init -> wp_loaded -> wp -> template_redirect -> get_header -> wp_enqueue_scripts -> wp_head
+	$action = 'template_redirect'; // OK
+	//$action = 'get_header';// NG in theme 'pinnacle'(use redux framework)
+}
+
+add_action($action, 'useso_take_over_google_obstart');
+function useso_take_over_google_obstart() {
+	ob_start('useso_take_over_google_obend');
+}
+
+function useso_take_over_google_obend($content) {
+	return useso_take_over_google_filter($content);
+}
+
+function useso_take_over_google_filter($content)
+{
+	$content = apply_filters('useso_take_over_google_content_filter_before', $content);
+
+	/*
+	<link rel="stylesheet" id="open-sans-css" href="//fonts.googleapis．com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&amp;subset=latin%2Clatin-ext&amp;ver=3.9.2" type="text/css" media="all">
+	<script type="text/javascript" src="http://ajax.googleapis．com/ajax/libs/jquery/1.6/jquery.min.js"></script>
+	*/
+	$regexp = "/<(link|script)([^<>]+)>/i";
+	$content = preg_replace_callback(
+		$regexp,
+		"useso_take_over_google_str_handler",
+		$content
+	);
+
+	/*
+	@import url(http://fonts.googleapis．com/css?family=Roboto+Condensed:regular);
+	@import url(http://fonts.googleapis．com/css?family=Merriweather:300,300italic,700,700italic);
+	*/
+	$regexp = "/@import\s+url\([^\(\)]+\);?/i";
+	$content = preg_replace_callback(
+		$regexp,
+		"useso_take_over_google_str_handler",
+		$content
+	);
+
+	/*
+	google fonts imported by 'Web Font Loader'
+	*/
+	$webfont_js = USESO_TAKE_OVER_GOOGLE_PLUGIN_URL.'webfont_v1.5.3.js';
+	if (is_ssl()) {
+		$webfont_js = USESO_TAKE_OVER_GOOGLE_PLUGIN_URL.'webfont_https_v1.5.3.js';
+	}
+	//$content = str_ireplace('//ajax.googleapis'.'.com/ajax/libs/webfont/1/webfont.js', substr($webfont_js, strpos($webfont_js,'//')), $content);
+	$content = preg_replace('|//ajax.googleapis'.'.com/ajax/libs/webfont/[\d\.]+/webfont.js|i', substr($webfont_js, strpos($webfont_js,'//')), $content);
+
+	/*
+	gravatar imgs:
+		<img src="http://1.gravatar．com/avatar/11fee321889526d1df2393655f48bd0c?s=26&d=retro&r=g">
+		<img src="https://secure.gravatar．com/avatar/06a2950d128ec9faf155e28d9e889baa?s=120">
+	*/
+	/* v1 bak
+	$regexp = "/(\d+|www|secure|cn).gravatar.com\/avatar/i";
+	$content = preg_replace($regexp, 'sdn.geekzu.org/avatar', $content);
+	*/
+	$regexp = "/<img([^<>]+)>/i";
+	$content = preg_replace_callback(
+		$regexp,
+		"useso_take_over_google_str_handler2",
+		$content
+	);
+
+	return apply_filters('useso_take_over_google_content_filter_after', $content);
+}
+
+function useso_take_over_google_str_handler($matches)
+{
+	$str = $matches[0];
+
+	if (!is_ssl()) {
+		$str = str_ireplace('//fonts.googleapis'.'.com/', '//fonts.useso.com/', $str);
+		$str = str_ireplace('//ajax.googleapis'.'.com/', '//ajax.useso.com/', $str);
+
+		//$str = str_ireplace('//fonts.googleapis'.'.com/', '//fonts.geekzu.org/', $str);
+		//$str = str_ireplace('//ajax.googleapis'.'.com/', '//fdn.geekzu.org/ajax/', $str);
+	} else {
+		//$str = str_ireplace('//fonts.googleapis'.'.com/', '//fonts.lug.ustc.edu.cn/', $str);
+		//$str = str_ireplace('//ajax.googleapis'.'.com/', '//ajax.lug.ustc.edu.cn/', $str);
+
+		$str = str_ireplace('//fonts.googleapis'.'.com/', '//fonts.geekzu.org/', $str);
+		$str = str_ireplace('//ajax.googleapis'.'.com/', '//sdn.geekzu.org/ajax/', $str);
+	}
+
+	/*
+	fix below references:
+		https://fonts.useso.com/....
+		https://ajax.useso.com/....
+	*/
+	if (!is_ssl()) {
+		$str = str_ireplace('https://fonts.useso.com/', 'http://fonts.useso.com/', $str);
+		$str = str_ireplace('https://ajax.useso.com/', 'http://ajax.useso.com/', $str);
+
+		$str = str_ireplace('https://fonts.geekzu.org/', 'http://fonts.geekzu.org/', $str);
+		$str = str_ireplace('https://fdn.geekzu.org/ajax/', 'http://fdn.geekzu.org/ajax/', $str);
+	} else {
+		//...
+	}
+
+	return $str;
+}
+
+function useso_take_over_google_str_handler2($matches)
+{
+	$str = $matches[0];
+
+	$regexp = "/(\d+|www|secure|cn|s).gravatar.com\/avatar/i";
+	$str = preg_replace($regexp, 'sdn.geekzu.org/avatar', $str);
+
+	return $str;
+}
