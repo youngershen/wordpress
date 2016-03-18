4aeca94995474b502689cee180ae2c06debc434a
youngershen <younger.x.shen@gmail.com>
2016-03-18 15:25:21 +0800

    init

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'youngershen');
 define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '898DFE###$');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
